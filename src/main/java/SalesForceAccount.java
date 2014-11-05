01a78fb41864e5ae5ca10645cc1a0d97b6636b3b
Jameson Triplett <jameson.triplett@mulesoft.com>
2014-11-05 14:25:05 -0800

    InitialCommit

---
+++ src/main/java/SalesForceAccount.java
@@ -0,0 +1,54 @@
+
+public class SalesForceAccount {
+	private String AccountName;
+	private String Phone;
+	private String Fax;
+	private String AccountType;
+	private String Industry;
+	private int AnnualIncome;
+	private String Website;
+	public String getAccountName() {
+		return AccountName;
+	}
+	public void setAccountName(String accountName) {
+		AccountName = accountName;
+	}
+	public String getPhone() {
+		return Phone;
+	}
+	public void setPhone(String phone) {
+		Phone = phone;
+	}
+	public String getFax() {
+		return Fax;
+	}
+	public void setFax(String fax) {
+		Fax = fax;
+	}
+	public String getAccountType() {
+		return AccountType;
+	}
+	public void setAccountType(String accountType) {
+		AccountType = accountType;
+	}
+	public String getIndustry() {
+		return Industry;
+	}
+	public void setIndustry(String industry) {
+		Industry = industry;
+	}
+	public int getAnnualIncome() {
+		return AnnualIncome;
+	}
+	public void setAnnualIncome(int annualIncome) {
+		AnnualIncome = annualIncome;
+	}
+	public String getWebsite() {
+		return Website;
+	}
+	public void setWebsite(String website) {
+		Website = website;
+	}
+	
+	
+}
